4bd2acda64e6bf1d5c4db49c8e02a03e4d836519
SoniPinjala <sonipinjala99@gmail.com>
2020-08-20 12:56:06 +0530

    added ask question

--- src/components/AskQuestion/AskQuestion.jsx
+++ src/components/AskQuestion/AskQuestion.jsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './AskQuestion.css';
 
-function AskQuestion() {
-	return (
-		<div>
-			<h1>AskQuestion</h1>
-		</div>
+export class AskQuestion extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {value: ''};  
+	  this.handleChange = this.handleChange.bind(this);
+	  this.handleSubmit = this.handleSubmit.bind(this);
+	}
+  
+	handleChange(event) {
+	//   this.setState({value: event.target.value});
+	}
 
-	);
-}
+	handleQuesChange(event) {
+		  this.setState({value: event.target.value});
+		  console.log(event.target.value);
+		}
+  
+	handleSubmit(event) {
+	  alert('A name was submitted: ' + this.state.value);
+	  event.preventDefault();
+	}
+  
+	render() {
+	  return (
+		<form onSubmit={this.handleSubmit}>
 
-export default AskQuestion;
+		  <div class="title">
+            <label>
+			<h5><b>Title:</b></h5>			
+			<input type="text" value={this.state.value} />
+			</label>
+		  </div>
+
+		  <br></br>
+
+		  <div class="QuesArea">
+			<label>
+				<h5><b>Your Question</b></h5>
+				<textarea>
+					<input type="textarea" placeholder = "Your Question Goes Here" onChange={this.handleQuesChange}/>
+				</textarea>				
+			</label>
+		  </div>
+
+	      <br></br>
+		  <div class="title">
+            <label>
+			<h5><b>Tags:</b></h5>			
+			<input type="text" placeholder = "add tags" value={this.state.value} />
+			</label>
+		  </div>
+
+		  <div>
+		  	<input type="submit" value="Post your Question" />
+		  </div>
+
+		</form>
+	  );
+	}
+  }
+
+
+  export default AskQuestion;
