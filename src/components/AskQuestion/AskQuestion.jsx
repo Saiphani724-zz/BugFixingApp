21517bcc50f69c986a949666e4bbf102132a3bdf
saiphani724 <saiphani724@gmail.com>
2020-08-28 23:03:38 +0530

    fixed bold

--- src/components/AskQuestion/AskQuestion.jsx
+++ src/components/AskQuestion/AskQuestion.jsx
@@ -1,66 +1,87 @@
-import React, { Component } from 'react';
+import React, { Component , PropTypes} from 'react';
 import './AskQuestion.scss';
 import { Form, Button } from 'react-bootstrap';
 import bulb from "./bestuu.png"
-import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import RichTextEditor from 'react-rte';
 
 export class AskQuestion extends Component {
-	// constructor(props) {
-	// 	super(props);
+  constructor(props) {
+    super(props);
+  
+  }
+    
+  state = {
+    value: RichTextEditor.createEmptyValue()
+   }
 
-	// }
+  onChange = (value) => {
+    this.setState({value});
+    if (this.props.onChange) {
+      // Send the changes up to the parent component as an HTML string.
+      // This is here to demonstrate using .toString() but in a real app it
+      // would be better to avoid generating a string on each change.
+      this.props.onChange(
+        value.toString('html')
+      );
+    }
+  };
 
+  render() {
+    const toolbarConfig = {
+      // Optionally specify the groups to display (displayed in the order listed).
+      display: ['INLINE_STYLE_BUTTONS', 'BLOCK_TYPE_BUTTONS', 'BLOCK_TYPE_DROPDOWN', 'HISTORY_BUTTONS'],
+      INLINE_STYLE_BUTTONS: [
+        {label: 'Bold', style: 'BOLD', className: 'custom-css-class'},
+        {label: 'Italic', style: 'ITALIC'},
+        {label: 'Underline', style: 'UNDERLINE'}
+      ],
+      BLOCK_TYPE_DROPDOWN: [
+        {label: 'Normal', style: 'unstyled'},
+        {label: 'Heading Large', style: 'header-one'},
+        {label: 'Heading Medium', style: 'header-two'},
+        {label: 'Heading Small', style: 'header-three'}
+      ],
+      BLOCK_TYPE_BUTTONS: [
+        {label: 'UL', style: 'unordered-list-item'},
+        {label: 'OL', style: 'ordered-list-item'}
+      ]
+      };
+    return (
+      <div class="imgQues">
+        <img src={bulb} alt="image"></img>
 
-	render() {
-		return (
-			<div class="imgQues">
-				<img src={bulb} alt="AskQuestion" ></img>
+      <div class="askQues">
+        <Form >
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label><b>Title</b></Form.Label>
+          <Form.Control type="email" placeholder="Enter the title here" />
+        </Form.Group>
 
-			<div class="askQues">
-				<Form >
-				<Form.Group controlId="exampleForm.ControlInput1">
-					<Form.Label><b>Title</b></Form.Label>
-					<Form.Control type="email" placeholder="Enter the title here" />
-				</Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label><b>Question area</b></Form.Label>
+          <RichTextEditor
+            toolbarConfig={toolbarConfig}
+            value={this.state.value}
+            onChange={this.onChange}
+            className= 'custom-css-class'
+          />
+        </Form.Group>
 
-				<Form.Group controlId="exampleForm.ControlTextarea1">
-					<Form.Label><b>Question area</b></Form.Label>
-					<CKEditor
-						editor={ ClassicEditor }
-						data=""
-						onInit={ editor => {
-							// You can store the "editor" and use when it is needed.
-							console.log( 'Editor is ready to use!', editor );
-						} }
-						onChange={ ( event, editor ) => {
-							const data = editor.getData();
-							console.log( { event, editor, data } );
-						} }
-						onBlur={ ( event, editor ) => {
-							console.log( 'Blur.', editor );
-						} }
-						onFocus={ ( event, editor ) => {
-							console.log( 'Focus.', editor );
-						} }
-                	/>
-				</Form.Group>
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label>Tags</Form.Label>
+          <Form.Control type="email" placeholder="Your tags go here" />
+        </Form.Group>
+        <br></br>
+        <Button variant="primary" type="submit">
+          Post your Question
+          </Button>
 
-				<Form.Group controlId="exampleForm.ControlInput1">
-					<Form.Label>Tags</Form.Label>
-					<Form.Control type="email" placeholder="Your tags go here" />
-				</Form.Group>
-				<br></br>
-				<Button variant="primary" type="submit">
-					Post your Question
-  				</Button>
-
-			</Form>
-			</div>
-			</div>
-			
-		);
-	}
+      </Form>
+      </div>
+      </div>
+      
+    );
+  }
 }
 
 
